5759ff0d153ca3d9cff804c9952f621670659d25
KIM-KWAN-IL <118953934+KIM-KWAN-IL@users.noreply.github.com>
2023-11-08 19:26:33 +0900

    3

--- src/sprint_sw_god.js
+++ src/sprint_sw_god.js
@@ -46,6 +46,20 @@ window.onload = function init() {
         color: 0xffffff
     });
 
+    //add items to scene
+	var numTrees=10;
+	var newTree;
+    
+	for(var i=0;i<numTrees;i++){
+		newTree=createTree();
+		newTree.position.z=i*-3;
+		newTree.rotation.z=(Math.random()*(2*Math.PI/10))+-Math.PI/10;
+        newTree.position.x = 50;  // 나무의 x 좌표를 boxGeometry의 x 좌표에 맞게 조정
+        newTree.position.y =0;   // 나무의 y 좌표를 boxGeometry의 위에 맞게 조정
+        
+		scene.add(newTree);
+	}
+
     const boxMeterial = new THREE.MeshBasicMaterial({ color: 'white' });
 
     // Mesh and Add to Camera
@@ -65,6 +79,8 @@ window.onload = function init() {
 
     // Render
     function render() {
+        
+
         rot += 0.1;
         // 각도 변환
         const radian = rot * (Math.PI / 180);
@@ -112,3 +128,86 @@ window.onload = function init() {
     // 초기화
     onResize();
 }
+function createTree(){
+	var sides=8;
+	var tiers=6;
+	var scalarMultiplier=(Math.random()*(0.25-0.1))+0.05;
+	var midPointVector= new THREE.Vector3();
+	var vertexVector= new THREE.Vector3();
+	var treeGeometry = new THREE.ConeGeometry( 0.5, 1, sides, tiers);
+	var treeMaterial = new THREE.MeshStandardMaterial( { color: 0x33ff33,shading:THREE.FlatShading  } );
+	var offset;
+	midPointVector=treeGeometry.vertices[0].clone();
+	var currentTier=0;
+	var vertexIndex;
+	blowUpTree(treeGeometry.vertices,sides,0,scalarMultiplier);
+	tightenTree(treeGeometry.vertices,sides,1);
+	blowUpTree(treeGeometry.vertices,sides,2,scalarMultiplier*1.1,true);
+	tightenTree(treeGeometry.vertices,sides,3);
+	blowUpTree(treeGeometry.vertices,sides,4,scalarMultiplier*1.2);
+	tightenTree(treeGeometry.vertices,sides,5);
+	var treeTop = new THREE.Mesh( treeGeometry, treeMaterial );
+	treeTop.castShadow=true;
+	treeTop.receiveShadow=false;
+	treeTop.position.y=0.9;
+	treeTop.rotation.y=(Math.random()*(Math.PI));
+	var treeTrunkGeometry = new THREE.CylinderGeometry( 0.1, 0.1,0.5);
+	var trunkMaterial = new THREE.MeshStandardMaterial( { color: 0x886633,shading:THREE.FlatShading  } );
+	var treeTrunk = new THREE.Mesh( treeTrunkGeometry, trunkMaterial );
+	treeTrunk.position.y=0.25;
+	var tree =new THREE.Object3D();
+	tree.add(treeTrunk);
+	tree.add(treeTop);
+    
+	return tree;
+}
+
+
+function blowUpTree(vertices,sides,currentTier,scalarMultiplier,odd){
+	var vertexIndex;
+	var vertexVector= new THREE.Vector3();
+	var midPointVector=vertices[0].clone();
+	var offset;
+	for(var i=0;i<sides;i++){
+		vertexIndex=(currentTier*sides)+1;
+		vertexVector=vertices[i+vertexIndex].clone();
+		midPointVector.y=vertexVector.y;
+		offset=vertexVector.sub(midPointVector);
+		if(odd){
+			if(i%2===0){
+				offset.normalize().multiplyScalar(scalarMultiplier/6);
+				vertices[i+vertexIndex].add(offset);
+			}else{
+				offset.normalize().multiplyScalar(scalarMultiplier);
+				vertices[i+vertexIndex].add(offset);
+				vertices[i+vertexIndex].y=vertices[i+vertexIndex+sides].y+0.05;
+			}
+		}else{
+			if(i%2!==0){
+				offset.normalize().multiplyScalar(scalarMultiplier/6);
+				vertices[i+vertexIndex].add(offset);
+			}else{
+				offset.normalize().multiplyScalar(scalarMultiplier);
+				vertices[i+vertexIndex].add(offset);
+				vertices[i+vertexIndex].y=vertices[i+vertexIndex+sides].y+0.05;
+			}
+		}
+	}
+}
+
+
+function tightenTree(vertices,sides,currentTier){
+	var vertexIndex;
+	var vertexVector= new THREE.Vector3();
+	var midPointVector=vertices[0].clone();
+	var offset;
+	for(var i=0;i<sides;i++){
+		vertexIndex=(currentTier*sides)+1;
+		vertexVector=vertices[i+vertexIndex].clone();
+		midPointVector.y=vertexVector.y;
+		offset=vertexVector.sub(midPointVector);
+		offset.normalize().multiplyScalar(0.06);
+		vertices[i+vertexIndex].sub(offset);
+	}
+}
+
