66e5305bb093be1fe17a35cb73ddcb60ac586214
Si-Ft <kshswel5@gmail.com>
2023-11-10 21:48:08 +0900

    Add trees on track

--- src/objects/roadsign.js
+++ src/objects/roadsign.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 //표지판을 생성하는 코드
 export function createRoadSign() {
-	const signRadius = 2;
+	const signRadius = 2.5;
 	const signHeight = 1;
 
 	const poleHeight = 3;
@@ -14,7 +14,7 @@ export function createRoadSign() {
 		const signMaterial = new THREE.MeshStandardMaterial({ color: 0x228b22, side: THREE.DoubleSide });
 		const signMesh = new THREE.Mesh(signGeometry, signMaterial);
 		signMesh.position.z += 0.3;
-		signMesh.position.y += 1;
+		signMesh.position.y += 2;
 
 		const poleGeometry = new THREE.CylinderGeometry(poleRadius, poleRadius, poleHeight, 32);
 		const poleMaterial = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
@@ -35,6 +35,13 @@ export function createRoadSign() {
 		sign.position.y += 3;
 		sign.rotation.y = Math.PI / 2;
 		signGroup.add(sign);
+		
+		const sign2 = createSign();
+		sign2.position.z = 7;
+		sign2.position.x = -i * 100;
+		sign2.position.y += 3;
+		sign2.rotation.y = Math.PI / 2;
+		signGroup.add(sign2);
 	}
 	
 	return signGroup;
